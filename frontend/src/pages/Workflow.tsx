bc5634cb85d74e7571e408142ce940b91206df79
GnohChengYi <chengyignoh@gmail.com>
2024-01-20 18:42:06 +0800

    dialog select pass back

--- frontend/src/pages/Workflow.tsx
+++ frontend/src/pages/Workflow.tsx
@@ -1,45 +1,88 @@
 import "../App.css";
-import { useState } from "react";
-import { Button, Paper, TextField } from '@mui/material';
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useEffect, useState } from "react";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    MenuItem,
+    Paper,
+    Select,
+    TextField
+} from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Draggable from "react-draggable";
 
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
-    typography: {
-        body1: {
-            color: '#ffffff'
-        }
-    }
-})
+
+type Engine = {
+    _id: string;
+    name: string;
+    status: any;
+}
+
 
 function WorkflowPage() {
     const [name, setName] = useState<string>("");
+    const [open, setOpen] = useState(false);
+    const [selectedEngine, setSelectedEngine] = useState<any>(null);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+        setSelectedEngine(null);
+    };
+    const handleClose = () => {
+        setOpen(false);
+        // do nothing after closing dialog if no engine is selected
+        if (selectedEngine === null) {
+            return;
+        }
+        // TODO add new engine job
+        setName("bla");
+    };
 
     return (
-        <ThemeProvider theme={darkTheme}>
-            <div className="workflow">
-                <div className="top-bar">
-                    <TextField
-                        placeholder="My New Workflow"
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <Button variant="contained">
-                        +
-                    </Button>
-                </div>
-                <Paper className="paper">
-                    paper
-                </Paper>
-                <Button>
-                    Execute
+        <div className="workflow">
+            <div className="top-bar">
+                <TextField
+                    variant="filled"
+                    placeholder="My New Workflow"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <Button
+                    variant="contained"
+                    onClick={handleClickOpen}
+                >
+                    +
                 </Button>
             </div>
-        </ThemeProvider>
+            <Paper className="paper">
+                paper
+            </Paper>
+            <Button>
+                Execute
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>{"Select a machine"}</DialogTitle>
+                <DialogContent>
+                    <Select onChange={(event) => setSelectedEngine(event.target.value)}>
+                        {useQuery(api.engine.get)?.map(object => (
+                            <MenuItem value={object._id} key={object._id}>
+                                {object.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Okay</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     )
 }
 
